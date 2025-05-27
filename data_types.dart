f9328f1b44d5def13f09e902a18b3e7d25b6cc3e
JuniorCarti <ridgejunior204@gmail.com>
2025-05-27 12:20:01 +0300

    Dart data_types

--- data_types.dart
+++ data_types.dart
@@ -5,3 +5,6 @@ int b = 20;
 int sum = a + b; // sum is 30
 int difference = b - a; // difference is 10
 int product = a * b; // product is 200
+int quotient = b ~/ a; // quotient is 2 (integer division)
+int remainder = b % a; // remainder is 0
+
