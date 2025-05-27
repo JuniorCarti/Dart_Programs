3377812ad91b39489c382ff616b328fb013182d9
JuniorCarti <ridgejunior204@gmail.com>
2025-05-27 12:20:33 +0300

    Dart data_types

--- data_types.dart
+++ data_types.dart
@@ -7,4 +7,13 @@ int difference = b - a; // difference is 10
 int product = a * b; // product is 200
 int quotient = b ~/ a; // quotient is 2 (integer division)
 int remainder = b % a; // remainder is 0
+// double numbers
+double x = 5.5;
+double y = 2.0;
+double sumDouble = x + y; // sumDouble is 7.5
+double differenceDouble = x - y; // differenceDouble is 3.5
+double productDouble = x * y; // productDouble is 11.0
+double quotientDouble = x / y; // quotientDouble is 2.75
+double remainderDouble = x % y; // remainderDouble is 1.5
+
 
