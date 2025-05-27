b1bd58d7784d10b1c97fa3f91074bcbeb9c71dc3
JuniorCarti <ridgejunior204@gmail.com>
2025-05-27 11:25:06 +0300

    variables in dart

---
+++ variables.dart
@@ -0,0 +1,21 @@
+//variables in dart
+void main() {
+  // Declaring variables
+  int age = 25; // Integer variable
+  double height = 5.9; // Double variable
+  String name = "Alice"; // String variable
+  bool isStudent = true; // Boolean variable
+  final String country = "USA"; // Final variable (constant)
+  //note that final variables cannot be changed after they are set
+
+  // Printing variables
+  print("Name: $name");
+  print("Age: $age");
+  print("Height: $height");
+  print("Is Student: $isStudent");
+
+  // Changing the value of a variable
+  age = 26;
+  print("Updated Age: $age");
+  
+}
