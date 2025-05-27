6a7d7423a7f0e9adbc829d613d55ba5e53f0385f
JuniorCarti <ridgejunior204@gmail.com>
2025-05-27 10:58:50 +0300

    first hello_world Program dart

--- hello_world.dart
+++ hello_world.dart
@@ -1,3 +1,12 @@
 void main() {
   print('Hello, World!');
 }
+
+//void: A return type. It tells Dart that this function does not return anything.
+// main: The name of the function. Dart programs start executing from the main function.
+// (): Parentheses. They indicate that this is a function.
+// print: A built-in function that outputs text to the console.
+// 'Hello, World!': A string literal. It is the text that will be printed to the console.
+// ;: A semicolon. It indicates the end of a statement in Dart.
+// This is a simple Dart program that prints "Hello, World!" to the console.
+
