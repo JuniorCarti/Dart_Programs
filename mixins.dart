915d2b844575d447f7bf5bced043fa097c474580
JuniorCarti <ridgejunior204@gmail.com>
2025-06-09 21:56:25 +0300

    mixins in dart

---
+++ mixins.dart
@@ -0,0 +1,4 @@
+//mixins in Dart are a way to add functionality to classes without using inheritance.
+//They allow you to define methods and properties that can be shared across multiple classes.
+// Mixins are a powerful feature in Dart that allows you to add functionality to classes without using inheritance.
+// They enable code reuse and help avoid the limitations of single inheritance.
