b41000bf4c318b92ed59a98b8b923d6acf378c44
JuniorCarti <ridgejunior204@gmail.com>
2025-06-09 20:52:00 +0300

    Inheritance in OOP

--- oop.dart
+++ oop.dart
@@ -26,16 +26,15 @@ void main() {
   Animal cat = Animal('Cat', 3);
   cat.speak(); // Output: Cat makes a sound.
 }
-class Person {
-  String name;
-  int age;
-
+//2. Inheritance
+class Dog extends Animal {
   // Constructor
-  Person(this.name, this.age);
+  Dog(String name, int age) : super(name, age);
 
-  // Method
-  void introduce() {
-    print('Hello, my name is $name and I am $age years old.');
+  // Overriding the speak method
+  @override
+  void speak() {
+    print('$name barks.');
   }
 }
 
