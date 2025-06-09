f6ad3b9a9f7ffb242a85142adf4ed03b3bebee18
JuniorCarti <ridgejunior204@gmail.com>
2025-06-09 20:37:46 +0300

    Object Oriented Programming in Dart

--- oop.dart
+++ oop.dart
@@ -1,1 +1,3 @@
-
+//Object-Oriented Programming (OOP) is a programming paradigm that uses "objects" 
+//to design applications and computer programs. Dart, 
+//as a modern programming language, has robust support for OOP concepts.
