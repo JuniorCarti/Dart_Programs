ba652fb852811a1cba831ddc80eed8d08fe96007
JuniorCarti <ridgejunior204@gmail.com>
2025-06-09 20:42:32 +0300

    Classes and Objects in OOP

--- oop.dart
+++ oop.dart
@@ -26,4 +26,16 @@ void main() {
   Animal cat = Animal('Cat', 3);
   cat.speak(); // Output: Cat makes a sound.
 }
+class Person {
+  String name;
+  int age;
+
+  // Constructor
+  Person(this.name, this.age);
+
+  // Method
+  void introduce() {
+    print('Hello, my name is $name and I am $age years old.');
+  }
+}
 
