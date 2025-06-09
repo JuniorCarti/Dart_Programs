e3e42fb85c369ab50616bfccd40820f5cadafd1c
JuniorCarti <ridgejunior204@gmail.com>
2025-06-09 21:13:19 +0300

    polymorhism in OOP

---
+++ polymorhism.dart
@@ -0,0 +1,7 @@
+//polymorphism.dart
+// This code snippet demonstrates polymorphism in Dart.
+// Polymorphism allows methods to do different things based on the object it is acting upon.
+//// In this example, we will create a base class `Shape` and
+/// two derived classes `Circle` and `Rectangle`.
+/// // The derived classes will override a method to calculate the area of the shape.
+/// // The base class will define a method that can be overridden by the derived classes.
