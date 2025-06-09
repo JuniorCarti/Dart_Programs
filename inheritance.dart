079ac719b97a9f797ccaf0272cb8298a1ce2077e
JuniorCarti <ridgejunior204@gmail.com>
2025-06-09 21:06:53 +0300

    inheritance in OOP

--- inheritance.dart
+++ inheritance.dart
@@ -3,3 +3,17 @@
 // Inheritance allows a class to inherit properties and methods from another class,
 // promoting code reuse and establishing a hierarchical relationship between classes.
 // In this example, we will create a base class `Vehicle` and two derived classes `Car` and `Bike`.
+// The derived classes will inherit properties and methods from the base class and 
+//can also have their own unique features.
+class Vehicle {
+  String brand;
+  int year;
+
+  // Constructor
+  Vehicle(this.brand, this.year);
+
+  // Method to display vehicle information
+  void displayInfo() {
+    print('Brand: $brand, Year: $year');
+  }
+}
