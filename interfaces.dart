ba34cacca4d61da3f662e16eb1cee9ed956ecf80
JuniorCarti <ridgejunior204@gmail.com>
2025-06-09 21:41:39 +0300

    interfaces in dart

---
+++ interfaces.dart
@@ -0,0 +1,2 @@
+//interfaces // In Dart, interfaces are a way to define a contract that classes can implement.
+// Interfaces allow you to define methods that must be implemented by any class that implements the interface.
