0e550a212b9507d2eef373630a01688ed6533867
JuniorCarti <ridgejunior204@gmail.com>
2025-06-09 21:13:50 +0300

    polymorhism in OOP

--- polymorhism.dart
+++ polymorhism.dart
@@ -5,3 +5,7 @@
 /// two derived classes `Circle` and `Rectangle`.
 /// // The derived classes will override a method to calculate the area of the shape.
 /// // The base class will define a method that can be overridden by the derived classes.
+/// // The derived classes will implement their own version of the method.
+/// // This allows us to call the same method on different objects and get different results.
+/// // The base class
+/// 
