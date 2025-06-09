7da559f2a0913e5763e7cf140e0fba050a347f89
JuniorCarti <ridgejunior204@gmail.com>
2025-06-09 22:27:50 +0300

    family constructors

--- factory_constructors.dart
+++ factory_constructors.dart
@@ -1,2 +1,5 @@
 //factory constructors.dart
 // This code snippet demonstrates the use of factory constructors in Dart.
+// Factory constructors are a special type of constructor that can return an instance of a class or a subclass.
+// They are useful when you want to control the instantiation process, such as 
+//returning a cached instance or creating an instance of a subclass based on some condition.
