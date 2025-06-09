83fc3ae442aab2e06c8a5528c0eb97dde7fda38f
JuniorCarti <ridgejunior204@gmail.com>
2025-06-09 22:37:29 +0300

    getters and setters

--- factory_constructors.dart
+++ factory_constructors.dart
@@ -3,3 +3,37 @@
 // Factory constructors are a special type of constructor that can return an instance of a class or a subclass.
 // They are useful when you want to control the instantiation process, such as 
 //returning a cached instance or creating an instance of a subclass based on some condition.
+class Animal {
+  String name;
+  
+  Animal(this.name);
+  
+  factory Animal.fromType(String type) {
+    if (type == 'dog') {
+      return Dog('Doggy');
+    } else if (type == 'cat') {
+      return Cat('Kitty');
+    }
+    throw ArgumentError('Unknown animal type');
+  }
+}
+
+class Dog extends Animal {
+  Dog(String name) : super(name);
+  
+  void bark() => print('Woof!');
+}
+
+class Cat extends Animal {
+  Cat(String name) : super(name);
+  
+  void meow() => print('Meow!');
+}
+
+void main() {
+  var dog = Animal.fromType('dog');
+  (dog as Dog).bark();
+  
+  var cat = Animal.fromType('cat');
+  (cat as Cat).meow();
+}
---
+++ getters_setters.dart
@@ -0,0 +1,3 @@
+//getters and setters
+// Getters and setters are used to access and modify the properties of a class.
+// They provide a way to encapsulate the internal state of an object and control how it is accessed or modified.
