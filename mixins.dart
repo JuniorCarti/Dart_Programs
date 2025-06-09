26dc5617fd65a6d407b97eada620c439a6adb411
JuniorCarti <ridgejunior204@gmail.com>
2025-06-09 21:56:53 +0300

    mixins in dart

--- mixins.dart
+++ mixins.dart
@@ -2,3 +2,9 @@
 //They allow you to define methods and properties that can be shared across multiple classes.
 // Mixins are a powerful feature in Dart that allows you to add functionality to classes without using inheritance.
 // They enable code reuse and help avoid the limitations of single inheritance.
+// Mixins can be applied to classes using the `with` keyword.
+mixin Logger {
+  void log(String message) {
+    print('Log: $message');
+  }
+}
