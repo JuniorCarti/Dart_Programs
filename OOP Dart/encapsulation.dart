77fcafc1f3b0e2d7650576aea8be79ce128f2a48
JuniorCarti <ridgejunior204@gmail.com>
2025-06-10 23:27:12 +0300

    encapsulation in OOP

--- OOP Dart/encapsulation.dart
+++ OOP Dart/encapsulation.dart
@@ -3,4 +3,9 @@
 //that restricts direct access to an object's data and methods.
 // It allows you to control how the data is accessed and modified, promoting data hiding and abstraction.
 // In Dart, encapsulation is achieved using access modifiers like `private` and `public`.
+//using encapsulation, you can protect the internal state of an object and expose 
+//only what is necessary through public methods.
+//private members are accessible only within the class, 
+//while public members can be accessed from outside the class.
+//In Dart, prefix a variable or method with an underscore _ to make it private to its library.
 
