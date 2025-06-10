e1736221bf302d9029e881b680c99a5f1295c028
JuniorCarti <ridgejunior204@gmail.com>
2025-06-10 23:00:12 +0300

    constructors in dart

--- OOP Dart/constructors.dart
+++ OOP Dart/constructors.dart
@@ -3,3 +3,19 @@
 //Defined using the class name and can have parameters to set initial values for the object's properties.
 //Default constructor: If no constructor is defined, Dart provides a default one that initializes all properties to 
 //their default values (null for objects, 0 for numbers, false for booleans).
+class Car {
+  String name;
+  String color;
+  String model;
+  int year;
+  String brand;
+
+  //DEFAULT CONSTRUCTOR
+  Car({
+    required this.name,
+    required this.color,
+    required this.model,
+    required this.year,
+    required this.brand,
+  });
+}
