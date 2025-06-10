dcf5eae50b78181d6e5d218fd75862dd7c076888
JuniorCarti <ridgejunior204@gmail.com>
2025-06-10 23:06:22 +0300

    Parameterized Constructor

--- OOP Dart/Parameterized_Constructor.dart
+++ OOP Dart/Parameterized_Constructor.dart
@@ -1,1 +1,30 @@
-//
+// parameterized constructor in Dart
+// A parameterized constructor allows you to pass arguments when creating an instance of a class.
+// This enables you to initialize the object's properties with specific values at the time of creation.
+// This is useful for creating objects with specific attributes without needing to
+// set them individually after creation.
+class Car {
+  String name;
+  String color;
+  String model;
+  int year;
+  String brand;
+
+  // Parameterized constructor
+  Car({
+    required this.name,
+    required this.color,
+    required this.model,
+    required this.year,
+    required this.brand,
+  });
+
+  // Method to display car details
+  void displayDetails() {
+    print('Car Name: $name');
+    print('Color: $color');
+    print('Model: $model');
+    print('Year: $year');
+    print('Brand: $brand');
+  }
+}
