4e2c57495a70940b75b0e4a3f318ee2b1ac50803
JuniorCarti <ridgejunior204@gmail.com>
2025-06-10 21:27:39 +0300

    classes

--- OOP Dart/classes&objects.dart
+++ OOP Dart/classes&objects.dart
@@ -1,2 +1,5 @@
 //classes and objects
 //understanding classes and objects in dart
+// a class is a blueprint for creating objects
+// an object is an instance of a class
+// a class can have properties and methods
