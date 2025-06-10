d780bb0b4bb8e55de093646bf384beaf0cdb87f3
JuniorCarti <ridgejunior204@gmail.com>
2025-06-10 23:25:16 +0300

    encapsulation in OOP

---
+++ OOP Dart/encapsulation.dart
@@ -0,0 +1,6 @@
+//encapsulation.dart
+// Encapsulation is a fundamental concept in object-oriented programming 
+//that restricts direct access to an object's data and methods.
+// It allows you to control how the data is accessed and modified, promoting data hiding and abstraction.
+// In Dart, encapsulation is achieved using access modifiers like `private` and `public`.
+
