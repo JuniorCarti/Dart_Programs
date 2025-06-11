d499bf28e7807798decafd958c5e36063db17940
JuniorCarti <ridgejunior204@gmail.com>
2025-06-11 21:47:22 +0300

    inheritance principle in dart

---
+++ Inheritance_OOP.dart
@@ -0,0 +1,4 @@
+//inheritance in Dart
+// allows a class to inherit properties and methods from another class, 
+//promoting code reuse and establishing a hierarchical relationship between classes.
+
--- static_members.dart
+++ static_members.dart
@@ -21,4 +21,6 @@ class MathUtils {
   static void resetLastCalculatedValue() {
     lastCalculatedValue = 0;
   }
+
+  
 }
