bcc381b6d7edc1883297425440a004a18e21b93d
JuniorCarti <ridgejunior204@gmail.com>
2025-06-11 22:54:54 +0300

    super in constructors

---
+++ OOP Dart/super_in_constructors.dart
@@ -0,0 +1,4 @@
+//using super in constructors
+// The `super` keyword can also be used in constructors to call the constructor of the parent class.
+// This is useful when the parent class has a constructor that requires parameters.
+
