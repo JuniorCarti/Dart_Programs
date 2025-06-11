263adadbaa372993b9946410df6cf25369879afe
JuniorCarti <ridgejunior204@gmail.com>
2025-06-11 22:49:44 +0300

    the super keyword

---
+++ OOP Dart/the_super_keyword.dart
@@ -0,0 +1,3 @@
+//the super keyword
+// The `super` keyword in Dart is used to refer to the parent class. 
+//It allows you to call methods or constructors of the parent class from within a child class.
