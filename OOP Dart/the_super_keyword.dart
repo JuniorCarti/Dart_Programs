211063f073c6b3d3bba1878856f4ba6b5bf9b542
JuniorCarti <ridgejunior204@gmail.com>
2025-06-11 22:53:43 +0300

    the super keyword

--- OOP Dart/the_super_keyword.dart
+++ OOP Dart/the_super_keyword.dart
@@ -3,3 +3,28 @@
 //It allows you to call methods or constructors of the parent class from within a child class.
 //used to access the parent class's properties and methods or constructors.
 //access overridden methods from the parent class.
+class Electronics {
+  // parent class
+  void display() { // Parent class method
+    print("Electronics display method"); // Output: Electronics display method
+  }
+}
+class Mobile extends Electronics {
+  // child class
+  @override // Annotation indicating method overriding
+  void display() { // Overriding the parent class method
+    print("Mobile display method"); // Output: Mobile display method
+  }
+
+  void showParentDisplay() {
+    super.display(); // Calling the parent class's display method
+  }
+}
+void main() {
+  Mobile mobile = Mobile(); // Creating an instance of the child class
+  mobile.display(); // Calling the overridden method from the child class
+  // Output: Mobile display method
+
+  mobile.showParentDisplay(); // Calling the parent class's display method using super
+  // Output: Electronics display method
+}
