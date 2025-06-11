878d2e2af27eb4fbbd643547351785efadbce83c
JuniorCarti <ridgejunior204@gmail.com>
2025-06-11 22:30:00 +0300

    inheritance in dart

--- OOP Dart/inheritance.dart
+++ OOP Dart/inheritance.dart
@@ -5,3 +5,21 @@
 //Child Class (Subclass): The class that inherits from the parent.
 //extends: The keyword used in Dart to create inheritance.
 //super: Refers to the parent class (used to call parent methods/constructors).
+//syntax inheritance in Dart
+class ParentClass {
+  //the properties of the parent class
+
+  //constructor of the parent class
+  ParentClass() {
+    //initialization code for parent class
+  }
+}
+class ChildClass extends ParentClass{
+  //the properties of the child class
+  //the child class can access the properties of the parent class
+  //the child class can also override the methods of the parent class
+  //the child class can also add new methods and properties
+  //the child class can also call the parent class constructor using super()
+  
+  ChildClass() : super(); //calling parent constructor
+}
