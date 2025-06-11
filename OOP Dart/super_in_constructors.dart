d639396a74db37009f20951348e83b99a8b895a9
JuniorCarti <ridgejunior204@gmail.com>
2025-06-11 22:56:46 +0300

    super in constructors

--- OOP Dart/super_in_constructors.dart
+++ OOP Dart/super_in_constructors.dart
@@ -2,3 +2,16 @@
 // The `super` keyword can also be used in constructors to call the constructor of the parent class.
 // This is useful when the parent class has a constructor that requires parameters.
 
+class Company {
+  String name;
+  int establishedYear;
+
+  // Parent class constructor
+  Company(this.name, this.establishedYear) {
+    print("Company: $name, Established: $establishedYear");
+  }
+}
+class TechCompany extends Company {
+  String techFocus;
+
+  // Child class constructor
