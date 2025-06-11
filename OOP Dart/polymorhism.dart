59778786d62cbbcd0aa450de498c68590396e7d2
JuniorCarti <ridgejunior204@gmail.com>
2025-06-11 23:06:30 +0300

    Polymorhism in dart

--- OOP Dart/polymorhism.dart
+++ OOP Dart/polymorhism.dart
@@ -1,2 +1,6 @@
 //polymorhism in dart
 // Polymorphism in Dart allows methods to do different things based on the object that it is acting upon.
+// It enables a single interface to represent different underlying forms (data types).
+// Key terms
+// - Polymorphism: The ability of different classes to be treated as instances of the same class through 
+//a common interface.
