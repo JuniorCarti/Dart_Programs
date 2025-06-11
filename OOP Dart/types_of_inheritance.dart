12a177db8766989439b553265d411cd852cf768a
JuniorCarti <ridgejunior204@gmail.com>
2025-06-11 22:31:57 +0300

    types of inheritance

---
+++ OOP Dart/types_of_inheritance.dart
@@ -0,0 +1,4 @@
+//the types of inheritance in dart
+// 1. Single Inheritance: A class inherits from one parent class.
+//Dart supports single inheritance (a class can inherit from only one parent) 
+//but allows multi-level inheritance (a child can further be a parent to another class).
