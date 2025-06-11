ed75e84ff3a1950411d2382aaec87f400548a14b
JuniorCarti <ridgejunior204@gmail.com>
2025-06-11 21:49:18 +0300

    inheritance principle in dart

--- Inheritance_OOP.dart
+++ Inheritance_OOP.dart
@@ -1,4 +1,11 @@
 //inheritance in Dart
 // allows a class to inherit properties and methods from another class, 
 //promoting code reuse and establishing a hierarchical relationship between classes.
+//Inheritance enables a new class to reuse, extend, or modify the behavior of an existing class.
+//Key Terms:
+//Parent Class (Superclass): The class being inherited from.
+//Child Class (Subclass): The class that inherits from the parent.
+//extends: The keyword used in Dart to create inheritance.
+//super: Refers to the parent class (used to call parent methods/constructors).
+
 
