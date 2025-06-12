f3819633262b5b38eb879e6bebc659812c103133
JuniorCarti <ridgejunior204@gmail.com>
2025-06-12 05:22:32 +0300

    types of polymorhism

---
+++ OOP Dart/types_of_polymorhism.dart
@@ -0,0 +1,6 @@
+//types of polymorphism in dart
+// There are two main types of polymorphism in Dart:
+// 1. Compile-time polymorphism (Method Overloading)
+// 2. Runtime polymorphism (Method Overriding)
+// Compile-time polymorphism is achieved through method overloading, 
+//where multiple methods can have the same name but different parameters.
