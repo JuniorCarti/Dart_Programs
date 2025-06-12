69e7beedf0ca871ace14883b01203acc12c1845b
JuniorCarti <ridgejunior204@gmail.com>
2025-06-12 05:42:54 +0300

    interface polymorhism example 2

--- OOP Dart/interface_polymorhism_example2.dart
+++ OOP Dart/interface_polymorhism_example2.dart
@@ -1,2 +1,5 @@
 //interface_polymorphism.dart
 //using abstract classes and interfaces
+abstract class NotificationService {
+  void sendNotification(String message); // Method to send a notification
+}
