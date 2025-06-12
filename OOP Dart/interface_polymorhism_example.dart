00252b8fd43697bcd54e58266fa8502ffb85c92a
JuniorCarti <ridgejunior204@gmail.com>
2025-06-12 05:38:29 +0300

    interface polymorhism example

--- OOP Dart/interface_polymorhism_example.dart
+++ OOP Dart/interface_polymorhism_example.dart
@@ -1,6 +1,16 @@
 //example of interface polymorphism in Dart
 //using abstract classes and interfaces
 abstract class Database {
-  void connect();
-  void disconnect();
+  void connect(); // Method to connect to the database
+  void disconnect(); // Method to disconnect from the database
+}
+class MySQLDatabase implements Database {
+  @override
+  void connect(){
+    print("Connecting to MySQL database...");
+  }
+  @override
+  void disconnect() {
+    print("Disconnecting from MySQL database...");
+  }
 }
