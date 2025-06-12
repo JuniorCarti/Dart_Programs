9ac1760467394fa08da844fe0ee8b3793a979580
JuniorCarti <ridgejunior204@gmail.com>
2025-06-12 23:59:18 +0300

    Add DomesticAirlineBooking extending KenyaAirwaysBookingSystem with custom flight messages

--- OOP Dart/airlinebooking_system.dart
+++ OOP Dart/airlinebooking_system.dart
@@ -1,5 +1,16 @@
 //abstraction.dart
-abstract class AirlineBookingSystem {
+abstract class KenyaAirwaysBookingSystem {
   void bookFlight(String flightNumber, String passengerName); // Abstract method to book a flight
   void cancelFlight(String flightNumber, String passengerName); // Abstract method to cancel a flight
+}
+class DomesticAirlineBooking extends KenyaAirwaysBookingSystem {
+  @override
+  void bookFlight(String flightNumber, String passengerName) {
+    print("Domestic flight $flightNumber booked for $passengerName.");
+  }
+
+  @override
+  void cancelFlight(String flightNumber, String passengerName) {
+    print("Domestic flight $flightNumber canceled for $passengerName.");
+  }
 }
