251ad727974c328e5dcfeec24da20f6c711b84b5
JuniorCarti <ridgejunior204@gmail.com>
2025-06-12 05:30:09 +0300

    interface polymorhism

---
+++ OOP Dart/interface_polymorphism.dart
@@ -0,0 +1,4 @@
+//interface_polymorphism.dart
+// Interface polymorphism in Dart allows different classes to implement the same interface,
+// enabling them to be treated interchangeably. This is achieved through the use of abstract classes or interfaces.
+
