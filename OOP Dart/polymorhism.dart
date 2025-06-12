f7f9c819379e960a43a7463b207990b2dfd978b1
JuniorCarti <ridgejunior204@gmail.com>
2025-06-12 05:20:03 +0300

    Polymorhism in dart

--- OOP Dart/polymorhism.dart
+++ OOP Dart/polymorhism.dart
@@ -4,3 +4,31 @@
 // Key terms
 // - Polymorphism: The ability of different classes to be treated as instances of the same class through 
 //a common interface.
+//basic syntax 
+class Animal {
+  void sound() {
+    print("Animal makes a sound");
+  }
+}
+class Dog extends Animal {
+  @override
+  void sound() {
+    print("Dog barks");
+  }
+}
+class Cat extends
+Animal {
+  @override
+  void sound() {
+    print("Cat meows");
+  }
+}
+void main() {
+  Animal myDog = Dog(); // Upcasting
+  Animal myCat = Cat(); // Upcasting
+  
+  myDog.sound(); // Outputs: Dog barks
+  myCat.sound(); // Outputs: Cat meows
+  
+}
+
