f3587d76446c0ce62ec4bb937c26dd2170cf1a27
JuniorCarti <ridgejunior204@gmail.com>
2025-06-12 05:33:03 +0300

    interface polymorhism

--- OOP Dart/interface_polymorphism.dart
+++ OOP Dart/interface_polymorphism.dart
@@ -8,7 +8,13 @@ abstract class PaymentProcessor { // Abstract class defining the interface for p
 }
 class PayPalProcessor implements PaymentProcessor { // Class implementing the PaymentProcessor interface
   @override
-  void processPayment(double amount) {
+  void processPayment(double amount) { // Implementation of the processPayment method for PayPal
     print("Processing payment of \$${amount} through PayPal.");
+  }
+}
+class StripeProcessor implements PaymentProcessor { // Class implementing the PaymentProcessor interface
+  @override
+  void processPayment(double amount) { // Implementation of the processPayment method for Stripe
+    print("Processing payment of \$${amount} through Stripe.");
   }
 }
