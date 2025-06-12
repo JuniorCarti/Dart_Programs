7c3e88b6b6787ad335cfef2f8acfbb2dffc7d57b
JuniorCarti <ridgejunior204@gmail.com>
2025-06-12 23:25:34 +0300

    Demonstrate abstraction using abstract classes and interfaces in Dart

---
+++ OOP Dart/abstraction.dart
@@ -0,0 +1,3 @@
+//Abstraction means showing only the necessary details and hiding the internal complexity.
+//It helps in reducing complexity and improving code maintainability.
+// In Dart, abstraction can be achieved using abstract classes and interfaces.
