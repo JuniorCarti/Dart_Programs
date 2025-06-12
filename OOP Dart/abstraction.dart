d521b832a867f65ba3b4b313e1de206c08a1fea1
JuniorCarti <ridgejunior204@gmail.com>
2025-06-12 23:34:47 +0300

    Demonstrate abstraction using abstract classes and interfaces in Dart

--- OOP Dart/abstraction.dart
+++ OOP Dart/abstraction.dart
@@ -10,3 +10,49 @@ abstract class Vehicle {
   void start(); // Abstract method to start the vehicle
   void stop(); // Abstract method to stop the vehicle
 }
+class Car extends Vehicle {
+  @override
+  void start() {
+    print("Car is starting..."); // Implementation of the start method for Car
+  }
+
+  @override
+  void stop() {
+    print("Car is stopping..."); // Implementation of the stop method for Car
+  }
+}
+class Bike extends Vehicle {
+  @override
+  void start() {
+    print("Bike is starting..."); // Implementation of the start method for Bike
+  }
+
+  @override
+  void stop() {
+    print("Bike is stopping..."); // Implementation of the stop method for Bike
+  }
+}
+class Truck extends Vehicle {
+  @override
+  void start() {
+    print("Truck is starting..."); // Implementation of the start method for Truck
+  }
+
+  @override
+  void stop() {
+    print("Truck is stopping..."); // Implementation of the stop method for Truck
+  }
+}
+void main() {
+  Vehicle car = Car(); // Creating an instance of Car
+  car.start(); // Output: Car is starting...
+  car.stop(); // Output: Car is stopping...
+
+  Vehicle bike = Bike(); // Creating an instance of Bike
+  bike.start(); // Output: Bike is starting...
+  bike.stop(); // Output: Bike is stopping...
+
+  Vehicle truck = Truck(); // Creating an instance of Truck
+  truck.start(); // Output: Truck is starting...
+  truck.stop(); // Output: Truck is stopping...
+}
