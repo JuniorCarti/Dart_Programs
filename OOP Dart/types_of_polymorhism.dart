06ce54187359363e8d58c9abd00405b2447822ab
JuniorCarti <ridgejunior204@gmail.com>
2025-06-12 05:23:20 +0300

    types of polymorhism

--- OOP Dart/types_of_polymorhism.dart
+++ OOP Dart/types_of_polymorhism.dart
@@ -4,3 +4,7 @@
 // 2. Runtime polymorphism (Method Overriding)
 // Compile-time polymorphism is achieved through method overloading, 
 //where multiple methods can have the same name but different parameters.
+// Runtime polymorphism is achieved through method overriding,
+//where a subclass provides a specific implementation of a method that is already defined in its superclass.
+// In Dart, polymorphism is primarily achieved through method overriding.
+
