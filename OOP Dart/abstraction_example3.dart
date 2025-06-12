744e43c1c4501ca3877f0e90128e8a822877d275
JuniorCarti <ridgejunior204@gmail.com>
2025-06-12 23:53:20 +0300

    Add Smart and Wearable FitnessTracker implementations using abstraction

--- OOP Dart/abstraction_example3.dart
+++ OOP Dart/abstraction_example3.dart
@@ -17,3 +17,42 @@ class BasicFitnessTracker implements FitnessTracker {
     return;
   }
 }
+class AdvancedFitnessTracker implements FitnessTracker {
+  @override
+  void trackSteps(int steps) {
+    print("Advanced tracking of steps: $steps");
+    return;
+  }
+
+  @override
+  void trackHeartRate(int heartRate) {
+    print("Advanced tracking of heart rate: $heartRate bpm");
+    return;
+  }
+}
+class SmartFitnessTracker implements FitnessTracker {
+  @override
+  void trackSteps(int steps) {
+    print("Smart tracking of steps: $steps with GPS");
+    return;
+  }
+
+  @override
+  void trackHeartRate(int heartRate) {
+    print("Smart tracking of heart rate: $heartRate bpm with ECG");
+    return;
+  }
+}
+class WearableFitnessTracker implements FitnessTracker {
+  @override
+  void trackSteps(int steps) {
+    print("Wearable tracking of steps: $steps with motion sensors");
+    return;
+  }
+
+  @override
+  void trackHeartRate(int heartRate) {
+    print("Wearable tracking of heart rate: $heartRate bpm with optical sensors");
+    return;
+  }
+}
