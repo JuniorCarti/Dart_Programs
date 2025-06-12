ada7e232adbebcec21feca9a8fca9af04838aade
JuniorCarti <ridgejunior204@gmail.com>
2025-06-12 05:32:13 +0300

    interface polymorhism

--- OOP Dart/interface_polymorphism.dart
+++ OOP Dart/interface_polymorphism.dart
@@ -2,3 +2,13 @@
 // Interface polymorphism in Dart allows different classes to implement the same interface,
 // enabling them to be treated interchangeably. This is achieved through the use of abstract classes or interfaces.
 
+//example of interface polymorphism
+abstract class PaymentProcessor { // Abstract class defining the interface for payment processing
+  void processPayment(double amount);
+}
+class PayPalProcessor implements PaymentProcessor { // Class implementing the PaymentProcessor interface
+  @override
+  void processPayment(double amount) {
+    print("Processing payment of \$${amount} through PayPal.");
+  }
+}
