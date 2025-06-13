ea564afed8a6fe599950a57f2b45eb9cbee89883
JuniorCarti <ridgejunior204@gmail.com>
2025-06-13 05:23:56 +0300

    mixins in OOP

--- OOP Dart/mixins.dart
+++ OOP Dart/mixins.dart
@@ -2,3 +2,6 @@
 // Mixins are a way to reuse a class's code in multiple class hierarchies.
 // They allow you to add functionality to classes without using inheritance.
 // Mixins are defined using the `mixin` keyword and can be applied to classes using the `with` keyword.
+//mixins allow a class to inherit properties and methods from multiple classes without using traditional inheritance
+//inheritance is a one-to-one relationship, while mixins allow for a one-to-many relationship
+//defining a mixin in Dart
