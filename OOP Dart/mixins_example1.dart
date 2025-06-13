795f156cecb59fcecb93ed6443fa35cad2847ce0
JuniorCarti <ridgejunior204@gmail.com>
2025-06-13 05:38:13 +0300

    mixins in OOP, logging functionality

--- OOP Dart/mixins_example1.dart
+++ OOP Dart/mixins_example1.dart
@@ -1,1 +1,8 @@
 //logging functionality(cross cutting concerns) using mixins
+//multiple classes (User Service, Product Service, Order Service) need logging 
+//but don't want to inherit from a common base class
+mixin Logger {
+  void log(String message) {
+    print("Log: $message - ${DateTime.now()}");
+  }
+}  
