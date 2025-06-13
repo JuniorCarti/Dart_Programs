95c806c5771236e11e1bf5fd1717031306365c36
JuniorCarti <ridgejunior204@gmail.com>
2025-06-13 05:44:11 +0300

    mixins in OOP, Payment Processing

--- OOP Dart/mixin_example2.dart
+++ OOP Dart/mixin_example2.dart
@@ -1,2 +1,9 @@
 //payment processing multiple behaviors using mixins
 // This example demonstrates how to use mixins in Dart to implement multiple behaviors
+//Different payment methods (PayPal, Stripe, M-Pesa, Bank Transfer) can be implemented using mixins
+mixin CardValidation {
+  bool validateCard(String cardNumber) {
+    // Simple validation logic for demonstration purposes
+    return cardNumber.length == 16;
+  }
+}
