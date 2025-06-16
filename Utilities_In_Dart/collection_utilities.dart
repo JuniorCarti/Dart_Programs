ae3ced0c66734b41252089c3a5828072538a250d
JuniorCarti <ridgejunior204@gmail.com>
2025-06-16 20:51:16 +0300

    collection utilities

--- Utilities_In_Dart/collection_utilities.dart
+++ Utilities_In_Dart/collection_utilities.dart
@@ -13,4 +13,21 @@ void main() {
   print(numbers.last); // 8
   print(numbers.isEmpty); // false
   print(numbers.isNotEmpty); // true
+
+  //transformation
+    // 1. map() – Transform Each Element
+  //The map() method is used to apply a function to each item in a list and return a new iterable.
+  print(numbers.map((n) => n * 2).toList());
+//map() takes every n in the list and returns n * n.
+//The result is a transformed list with each element squared.
+
+//2. where() – Filter Elements
+//The where() method filters the list based on a condition.
+//Only elements that satisfy the condition (n % 2 == 0) are kept.
+  print(numbers.where((n) => n % 2 == 0).toList());
+  print(numbers.reduce((a, b) => a + b)); // 56 (sum)
+
+
+
+
 }
