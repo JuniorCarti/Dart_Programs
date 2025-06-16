34bbf101e98718791ee8c09b515b03af4887e13d
JuniorCarti <ridgejunior204@gmail.com>
2025-06-16 21:31:44 +0300

    Asynchronous programming

---
+++ Utilities_In_Dart/Asynchronous Programming.dart
@@ -0,0 +1,8 @@
+//Asynchronous programming allows your code to perform tasks 
+//without waiting for other tasks to complete. It's like saying:
+//Do this task, and while you’re at it, I’ll go do something else and come back when you’re done.”
+//This is especially useful for tasks that take time to complete, like:
+//Fetching data from the internet
+//Reading/writing files
+//Waiting for user input
+//Making API requests
