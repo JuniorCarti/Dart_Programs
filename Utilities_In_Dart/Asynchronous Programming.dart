07551ce10cc5e37f689fab1b07630b03ba66a743
JuniorCarti <ridgejunior204@gmail.com>
2025-06-16 21:43:39 +0300

    Asynchronous programming

--- Utilities_In_Dart/Asynchronous Programming.dart
+++ Utilities_In_Dart/Asynchronous Programming.dart
@@ -1,4 +1,4 @@
-//Asynchronous programming allows your code to perform tasks 
+//Asynchronous programming allows your code to perform tasks
 //without waiting for other tasks to complete. It's like saying:
 //Do this task, and while you’re at it, I’ll go do something else and come back when you’re done.”
 //This is especially useful for tasks that take time to complete, like:
@@ -10,20 +10,22 @@
 //Synchronous Code:
 //Code is executed line by line.
 //Each task waits for the previous one to finish.
-void main() {
+
+  //Asynchronous Code:
+  //Code doesn't wait for tasks to finish.
+  //It can continue with other operations while a task is still running in the background.
+
+  //1. Future: Represents a value that will be available in the future.
+Future<String> fetchData() {
+    return Future.delayed(Duration(seconds: 10), () => 'Data loaded');
+  }
+void main() async {
   print("Start");
+  String Data = await fetchData();
   print("Processing...");
   print("End");
 
-//Asynchronous Code:
-//Code doesn't wait for tasks to finish.
-//It can continue with other operations while a task is still running in the background.
-
-
-//1. Future: Represents a value that will be available in the future.
-Future<String> fetchData() {
-  return Future.delayed(Duration(seconds: 2), () => 'Data loaded');
+  //2. async & await
+  //async: Marks a function as asynchronous.
+  //await: Waits for a future to complete without blocking the rest of the program.
 }
-
-}
-
