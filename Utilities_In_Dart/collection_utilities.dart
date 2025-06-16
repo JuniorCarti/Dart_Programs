29a3d2df4751ed3a96b9011f4198458bea934e28
JuniorCarti <ridgejunior204@gmail.com>
2025-06-16 20:35:01 +0300

    collection utilities

--- Utilities_In_Dart/collection_utilities.dart
+++ Utilities_In_Dart/collection_utilities.dart
@@ -2,5 +2,11 @@ void main() {
   //basic list operations
   List<int> numbers = [1, 2, 3, 4];
   numbers.add(5);
-  
+  numbers.addAll([2, 6, 7]);
+  numbers.insert(0, 0);
+  numbers.removeAt(1);
+
+  //querring
+
+  print(numbers);
 }
