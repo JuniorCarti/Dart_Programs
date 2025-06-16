d810154c512c264a0b2ac86e44e4c2772ee033a1
JuniorCarti <ridgejunior204@gmail.com>
2025-06-16 22:23:14 +0300

    enums

--- Utilities_In_Dart/enums.dart
+++ Utilities_In_Dart/enums.dart
@@ -1,2 +1,20 @@
 //An enum is a special type that defines a group of named constant values.
 //Use enums when a variable should only take one out of a small, fixed set of options
+enum Day { Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday }
+
+enum Weather { sunny, rainy, cloudy, snowy }
+
+void main() {
+  Weather today = Weather.sunny;
+
+  if (today == Weather.sunny) {
+    print("It's a sunny day!");
+  } else {
+    print("The weather isn't sunny");
+
+    // Looping through enum values
+    for (var w in Weather.values) {
+      print("Weather type: $w");
+    }
+  }
+}
