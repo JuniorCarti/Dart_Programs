ba67e9d3aac5b7775a6b8e542074d540a2d4ef99
JuniorCarti <ridgejunior204@gmail.com>
2025-06-16 21:13:15 +0300

    Map operations

--- Utilities_In_Dart/map utilities.dart
+++ Utilities_In_Dart/map utilities.dart
@@ -2,4 +2,7 @@ void main() {
   //map operations
   Map<String, int> ages = {'Alice': 20, 'Njenga': 30, 'Kimberly': 18};
   //Accessing the map
+
+  print(ages["Alice"]);
+  print(ages['Njenga']);
 }
