b29d8ca55ed8f0e90bf4367b5c272fc0ab313746
JuniorCarti <ridgejunior204@gmail.com>
2025-06-16 20:55:43 +0300

    collection utilities

--- Utilities_In_Dart/collection_utilities.dart
+++ Utilities_In_Dart/collection_utilities.dart
@@ -1,3 +1,5 @@
+import 'dart:collection';
+
 void main() {
   //basic list operations
   List<int> numbers = [1, 2, 3, 4];
@@ -15,19 +17,19 @@ void main() {
   print(numbers.isNotEmpty); // true
 
   //transformation
-    // 1. map() – Transform Each Element
+  // 1. map() – Transform Each Element
   //The map() method is used to apply a function to each item in a list and return a new iterable.
   print(numbers.map((n) => n * 2).toList());
-//map() takes every n in the list and returns n * n.
-//The result is a transformed list with each element squared.
+  //map() takes every n in the list and returns n * n.
+  //The result is a transformed list with each element squared.
 
-//2. where() – Filter Elements
-//The where() method filters the list based on a condition.
-//Only elements that satisfy the condition (n % 2 == 0) are kept.
+  //2. where() – Filter Elements
+  //The where() method filters the list based on a condition.
+  //Only elements that satisfy the condition (n % 2 == 0) are kept.
   print(numbers.where((n) => n % 2 == 0).toList());
-  print(numbers.reduce((a, b) => a + b)); // 56 (sum)
 
-
-
-
+  //➕ 3. reduce() – Reduce to a Single Value
+  //reduce() combines elements into one single value, such as a sum or max.
+  print(numbers.reduce((a, b) => a + b)); // 29 (sum)
+  print(List);
 }
