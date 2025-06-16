1ee3e094341c7d841b2f5ce86dd83709bd8bc9ba
JuniorCarti <ridgejunior204@gmail.com>
2025-06-16 21:48:33 +0300

    Asynchronous programming

--- Utilities_In_Dart/Asynchronous Programming example2.dart
+++ Utilities_In_Dart/Asynchronous Programming example2.dart
@@ -2,3 +2,14 @@
 //Start downloading a file.
 //You continue working in your app.
 //Once the download finishes, the file is available.
+Future<void> downloadFile() async {
+  print("Starting download...");
+  await Future.delayed(Duration(seconds: 15));
+  print("✅ File downloaded!");
+}
+
+void main() async {
+  print("Opening app...");
+  await downloadFile();
+  print("Now displaying the file.");
+}
